8af62b4fcf5c47f6bd8ed68a17b8b8998e502a0c
Gautham Srinivas <gsri0828@uni.sydney.edu.au>
2023-11-17 08:40:06 +0530

    added config

---
+++ app/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        cityguard: 'cityguard.html',
+        fieldwork: 'fieldwork.html',
+        foodpile: 'foodpile.html',
+        heatscape: 'heatscape.html'
+      }
+    }
+  }
+});
